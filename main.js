c1050c17bafc3331b5a59c555dda4e4cb7968add
GitDaniR <dani.rachev@gmail.com>
2023-08-23 15:37:10 +0300

    Added comments

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ let primes = [2];
 
 let step = 1;
 let stepsBeforeTurn = 1;
-let incStepsBeforeTurn = true;
 let direction = 0;
 let stepSize = 20;
 
@@ -20,6 +19,7 @@ function init() {
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
 
+    // Set pointer to center of canvas
     currentX = canvas.width / 2;
     currentY = canvas.height / 2;
     previousX = currentX;
@@ -39,12 +39,14 @@ function draw() {
     context.lineWidth = 3;
     context.strokeStyle = "azure";
 
+    // Draw line between previous and current pointers
     context.beginPath();
     context.moveTo(previousX, previousY);
     context.lineTo(currentX, currentY);
     context.stroke();
     context.closePath();
 
+    // Draw circle if the current number is a prime
     if (isPrime(step)) {
         context.beginPath();
         context.arc(currentX, currentY, 10, 0, 2 * Math.PI);
@@ -55,6 +57,13 @@ function draw() {
     doStep();
 }
 
+/* Method which checks if a given number is a prime
+* by dividing it by the previous prime numbers, less than
+* or equal to the square root of the number.
+* If the nubmer is a prime it is added to the list of
+* other primes.
+* @param num - the number to check
+*/ 
 function isPrime(num) {
     if (num == 1) return false;
     if (num == 2) return true;
@@ -69,9 +78,11 @@ function isPrime(num) {
 }
 
 function doStep() {
+    // Store previous pointer location
     previousX = currentX;
     previousY = currentY;
 
+    // Move pointer
     switch (direction) {
         case 0: currentX += stepSize; break; // RIGHT
         case 1: currentY -= stepSize; break; // UP
@@ -79,10 +90,12 @@ function doStep() {
         case 3: currentY += stepSize; break; // DOWN
     }
 
+    // Check if the pointer needs to turn
     if (step % stepsBeforeTurn == 0) {
-        incStepsBeforeTurn = !incStepsBeforeTurn;
-        stepsBeforeTurn += incStepsBeforeTurn;
-        direction = (direction + 1) % 4;
+        stepsBeforeTurn += direction % 2; // Increment only if the pointer moved UP or DOWN previously
+        direction = (direction + 1) % 4; // Change direction: RIGHT -> UP -> LEFT -> DOWN -> RIGHT...
     }
+
+    // Increment step
     ++step;
 }
